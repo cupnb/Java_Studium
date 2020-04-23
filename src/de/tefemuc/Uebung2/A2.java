6e3b04f75d196ed96e5dcbae7ff7c5cb724da6e8
Noah Boeckmann <noahboeckmann@gmx.net>
2020-04-23 11:54:23 +0200

    Blatt1A2 implementiert

---
+++ src/de/tefemuc/Uebung2/A2.java
@@ -0,0 +1,50 @@
+package de.tefemuc.Uebung2;
+
+public class A2 {
+    public static void a(){
+        System.out.println("Aufgabe 2a");
+        for(int i = 0; i < 10000; i++){
+            System.out.print("a");
+        }
+        System.out.println("\n");
+    }
+
+    public static void newline(){
+        System.out.println("Aufgabe 2b");
+        System.out.println("\\n\n");
+    }
+
+    public static void randBool(){
+        System.out.println("Aufgabe 2c");
+        double r = Math.random();
+        if (r < 0.5){
+            System.out.println("0\n");
+        } else {
+            System.out.println("1\n");
+        }
+    }
+
+    public static void randCount(){
+        System.out.println("Aufgabe 2d");
+        int count = 0;
+        double r;
+        while (!((r = Math.random()) < 0.1 || r > 0.9)){
+            count++;
+        }
+
+        System.out.println("Anzahl der Zufallszahlen: " + count + "\n");
+    }
+
+    public static void rand099(){
+        System.out.println("Aufgabe 2e");
+        int count = 0;
+        for (int i = 0; i < 1000000; i++){
+            if (Math.random() > 0.99){
+                count++;
+            }
+        }
+        System.out.println("Anzahl der Zufallszahlen über 0.99: " + count + "\n");
+    }
+
+
+}
---
+++ src/de/tefemuc/Uebung2/main.java
@@ -0,0 +1,11 @@
+package de.tefemuc.Uebung2;
+
+public class main {
+    public static void main(String[] args){
+        A2.a();
+        A2.newline();
+        A2.randBool();
+        A2.randCount();
+        A2.rand099();
+    }
+}
